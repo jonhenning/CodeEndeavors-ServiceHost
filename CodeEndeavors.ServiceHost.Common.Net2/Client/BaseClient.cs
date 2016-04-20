a56118a908e59ede7b6d8823b90062f0c9bcec7b
jonhenning <jonhenning@gmail.com>
2016-04-20 14:39:14 -0500

    ENH:  added BaseClient Since we are no longer encouraging ILMerge against CodeEndeavors.ServiceHost.Common we are fine with having a base client class to encapsulate common logic.

---
+++ CodeEndeavors.ServiceHost.Common.Net2/Client/BaseClient.cs
@@ -0,0 +1,52 @@
+﻿using CodeEndeavors.ServiceHost.Extensions;
+using CodeEndeavors.ServiceHost.Common.Services;
+using System;
+using System.Diagnostics;
+using Logger = CodeEndeavors.ServiceHost.Common.Services.Logging;
+
+namespace CodeEndeavors.ServiceHost.Common.Client
+{
+    public abstract class BaseClient
+    {
+        public BaseClient()
+        {
+            Helpers.HandleAssemblyResolve();
+        }
+
+        public BaseClient(string httpServiceUrl, int requestTimeout, string restfulServerExtension)
+        {
+            Helpers.HandleAssemblyResolve();
+        }
+        public BaseClient(string httpServiceUrl, int requestTimeout, string restfulServerExtension, string httpUser, string httpPassword, string authenticationType)
+        {
+            Helpers.HandleAssemblyResolve();
+        }
+
+        public abstract void SetAquireUserIdDelegate(Func<string> func);
+
+        public void ConfigureLogging(string logLevel, Action<string, string> onLoggingMessage)
+        {
+            Logger.LogLevel = logLevel.ToType<Logger.LoggingLevel>();
+            Logger.OnLoggingMessage += (Logger.LoggingLevel level, string message) =>
+            {
+                if (onLoggingMessage != null)
+                    onLoggingMessage(level.ToString(), message);
+            };
+        }
+
+        public static void Register(string url, int requestTimeout)
+        {
+            var st = new StackTrace();
+            var type = st.GetFrame(1).GetMethod().DeclaringType;
+
+            //var type = MethodBase.GetCurrentMethod().DeclaringType;
+
+            var mi = typeof(ServiceLocator).GetMethod("Register", new Type[] { typeof(string), typeof(int) });
+            var method = mi.MakeGenericMethod(type);
+            method.Invoke(null, new object[] { url, requestTimeout });
+
+            //ServiceLocator.Register<Client.PowerNETAccount>(url, requestTimeout);
+        }
+
+    }
+}
---
+++ CodeEndeavors.ServiceHost.Common/Client/BaseClient.cs
@@ -0,0 +1,55 @@
+﻿using CodeEndeavors.Extensions;
+using CodeEndeavors.ServiceHost.Common.Services;
+using System;
+using System.Diagnostics;
+using Logger = CodeEndeavors.ServiceHost.Common.Services.Logging;
+
+namespace CodeEndeavors.ServiceHost.Common.Client
+{
+    public abstract class BaseClient
+    {
+        public BaseClient()
+        {
+            Helpers.HandleAssemblyResolve();
+            //_service = new Stubs.Sales();
+        }
+
+        public BaseClient(string httpServiceUrl, int requestTimeout, string restfulServerExtension)
+        {
+            Helpers.HandleAssemblyResolve();
+            //_service = new Http.PowerNETAccount(httpServiceUrl, requestTimeout, restfulServerExtension);
+        }
+        public BaseClient(string httpServiceUrl, int requestTimeout, string restfulServerExtension, string httpUser, string httpPassword, string authenticationType)
+        {
+            Helpers.HandleAssemblyResolve();
+            //_service = new Http.PowerNETAccount(httpServiceUrl, requestTimeout, restfulServerExtension, httpUser, httpPassword, authenticationType);
+        }
+
+        public abstract void SetAquireUserIdDelegate(Func<string> func);
+
+        public void ConfigureLogging(string logLevel, Action<string, string> onLoggingMessage)
+        {
+            Logger.LogLevel = logLevel.ToType<Logger.LoggingLevel>();
+            Logger.OnLoggingMessage += (Logger.LoggingLevel level, string message) =>
+            {
+                if (onLoggingMessage != null)
+                    onLoggingMessage(level.ToString(), message);
+            };
+        }
+
+        public static void Register(string url, int requestTimeout)
+        {
+            var st = new StackTrace();
+            var type = st.GetFrame(1).GetMethod().DeclaringType;
+
+            //var type = MethodBase.GetCurrentMethod().DeclaringType;
+
+            var mi = typeof(ServiceLocator).GetMethod("Register", new Type[] { typeof(string), typeof(int) });
+            var method = mi.MakeGenericMethod(type);
+            method.Invoke(null, new object[] { url, requestTimeout });
+
+            //ServiceLocator.Register<Client.PowerNETAccount>(url, requestTimeout);
+        }
+
+    }
+}
