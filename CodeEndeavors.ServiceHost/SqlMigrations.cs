a71efcc36a10363d04a813a5291f1d69edcd4133
jonhenning <jonhenning@gmail.com>
2016-05-04 16:44:26 -0500

    ENH: support alternate schema for __schema_version table

--- CodeEndeavors.ServiceHost/SqlMigrations.cs
+++ CodeEndeavors.ServiceHost/SqlMigrations.cs
@@ -27,10 +27,15 @@ namespace CodeEndeavors.ServiceHost
         public static void Migrate(string connection)
         {
             var assembly = Assembly.GetCallingAssembly();
-
             Logging.Log(Logging.LoggingLevel.Info, "SqlMigrations.Migrate: " + assembly.FullName);
+            migrateSchema(assembly, connection, "dbo");
+        }
 
-            migrateSchema(assembly, connection);
+        public static void Migrate(string connection, string databaseSchemaForVersionTable)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+            Logging.Log(Logging.LoggingLevel.Info, "SqlMigrations.Migrate: " + assembly.FullName);
+            migrateSchema(assembly, connection, databaseSchemaForVersionTable);
         }
 
         private static DataTable getData(string sql, SqlConnection connection)
@@ -65,17 +70,20 @@ namespace CodeEndeavors.ServiceHost
             }
         }
 
-        private static void ensureSchema(Assembly assembly, SqlConnection connection)
+        private static void ensureSchema(Assembly assembly, SqlConnection connection, string databaseSchemaForVersionTable)
         {
             Logging.Log(Logging.LoggingLevel.Debug, "ensuring Schema for assembly: " + assembly.FullName);
 
-            var versionTable = "if not exists (select 1 from sysobjects where name = '" + migrationTableName + "') CREATE TABLE " + migrationTableName + " (tenant varchar(20), version int)";
+            var schemaSql = string.Format("IF NOT EXISTS (SELECT schema_name FROM INFORMATION_SCHEMA.SCHEMATA  WHERE schema_name = '{0}' ) BEGIN exec('create schema {0}') END", databaseSchemaForVersionTable);
+            executeSql(schemaSql, connection);
+
+            var versionTable = string.Format("if not exists (select 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{0}' AND TABLE_NAME = '{1}') CREATE TABLE [{0}].{1} (tenant varchar(20), version int)", databaseSchemaForVersionTable, migrationTableName);
             executeSql(versionTable, connection);
 
             var tenants = getTenants(assembly);
             foreach (var tenant in tenants)
             {
-                var versionSeed = "if not exists(select 1 from " + migrationTableName + " WHERE tenant = '" + tenant + "') INSERT INTO " + migrationTableName + " (tenant, version) VALUES ('" + tenant + "', 0)";
+                var versionSeed = string.Format("if not exists(select 1 from [{0}].{1} WHERE tenant = '{2}') INSERT INTO [{0}].{1} (tenant, version) VALUES ('{2}', 0)", databaseSchemaForVersionTable, migrationTableName, tenant);
                 executeSql(versionSeed, connection);
             }
         }
@@ -100,18 +108,18 @@ namespace CodeEndeavors.ServiceHost
             return names.Select(n => Resources.GetText(n, assembly)).ToList();
         }
 
-        private static void migrateSchema(Assembly assembly, string connectionString)
+        private static void migrateSchema(Assembly assembly, string connectionString, string databaseSchemaForVersionTable)
         {
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
 
-                ensureSchema(assembly, connection);
+                ensureSchema(assembly, connection, databaseSchemaForVersionTable);
 
                 var tenants = getTenants(assembly);
                 foreach (var tenant in tenants)
                 {
-                    var currentVersion = getSchemaVersion(tenant, connection);
+                    var currentVersion = getSchemaVersion(tenant, connection, databaseSchemaForVersionTable);
                     var versions = getTenantVersions(tenant, assembly);
                     var maxVersion = versions.Max();
 
@@ -125,24 +133,24 @@ namespace CodeEndeavors.ServiceHost
                             scripts.ForEach(s => executeSql(s, connection));
 
                             Logging.Log(Logging.LoggingLevel.Info, "Updating schema {0} version to {1}, executing {2} scripts", tenant, version, scripts.Count);
-                            updateSchemaVersion(tenant, version, connection);
+                            updateSchemaVersion(tenant, version, connection, databaseSchemaForVersionTable);
                         }
                     }
                 }
             }
         }
 
-        private static int getSchemaVersion(string tenant, SqlConnection connection)
+        private static int getSchemaVersion(string tenant, SqlConnection connection, string databaseSchemaForVersionTable)
         {
-            var dt = getData("SELECT version from " + migrationTableName + " WHERE tenant = '" + tenant + "'", connection);
+            var dt = getData(string.Format("SELECT version from [{0}].{1} WHERE tenant = '{2}'", databaseSchemaForVersionTable, migrationTableName, tenant), connection);
             if (dt.Rows.Count > 0)
                 return int.Parse(dt.Rows[0]["version"].ToString());
             return 0;
         }
-        private static void updateSchemaVersion(string tenant, int version, SqlConnection connection)
+        private static void updateSchemaVersion(string tenant, int version, SqlConnection connection, string databaseSchemaForVersionTable)
         {
             Logging.Log(Logging.LoggingLevel.Debug, "updateSchemaVersion({0}, {1})", tenant, version);
-            executeSql("UPDATE " + migrationTableName + " SET Version = @Version WHERE tenant = @tenant", new Dictionary<string, object>() { { "@Version", version }, { "@tenant", tenant } }, connection);
+            executeSql(string.Format("UPDATE [{0}].{1} SET Version = @Version WHERE tenant = @tenant", databaseSchemaForVersionTable, migrationTableName), new Dictionary<string, object>() { { "@Version", version }, { "@tenant", tenant } }, connection);
         }
 
     }
